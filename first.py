e4fa7650758dafdfea2303e1a8a27d2142e09edb
Fahad Shah <fahadahah30@gmail.com>
2024-11-05 17:44:59 +0500

    Add files via upload
    Python V.S code

---
+++ first.py
@@ -0,0 +1,34 @@
+number1=5
+number2=7
+number3=10
+#sum
+sum=number1+number2+number3
+print("sum:")
+#diffrence
+difrence=number1-number2-number3
+print("diffrence:")
+#product
+product=number1*number2*number3
+print("product:")
+#average
+average=(number1+number2+number3)/3
+print("average:")
+
+#triangle area 
+base=5
+heigth=10
+area_triangle=0.5*base*heigth
+print("area triangle:")
+#square
+side=6
+areaa_square=side*side
+print("araa square:")
+#circle area
+pi=3.14
+radius=10
+area_circle=pi*radius*radius
+print("area circle:")
+#area rectangle 
+lenght=10
+width=20
+area_rectangle=lenght*width
